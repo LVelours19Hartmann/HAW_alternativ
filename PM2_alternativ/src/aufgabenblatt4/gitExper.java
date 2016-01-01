90d7367b56b76bdbb61e9957f94a6d9237a78fb3
Lennart Hartmann <lennart_hartmann@yahoo.de>
2016-01-01 19:32:59 +0100

    Nur zum Testen

--- PM2_alternativ/src/aufgabenblatt4/gitExper.java
+++ PM2_alternativ/src/aufgabenblatt4/gitExper.java
@@ -3,6 +3,7 @@ package aufgabenblatt4;
 public class gitExper {
 	static String text = "gitExp test";
 	public static void main(String[] args) {
+		text+=": erfolg";
 		System.out.println(text);
 	}
 }
